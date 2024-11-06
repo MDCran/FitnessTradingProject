b88029ba6a0f97018591468dc73aeb1d638c2567
pleber2021 <lebercontact@gmail.com>
2024-11-06 16:06:33 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,40 +1,13 @@
 import PageWrapper from "src/components/PageWrapper";
-import { Table } from 'geist/components';
 
 
 const Rank = () => (
   <PageWrapper title="Rank">
     <p className="tc f4 fw4 w-70">
       LEADERBOARD
-      For testing
+      For testing table
     </p>
 
-    <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.Head>Col 1</Table.Head>
-          <Table.Head>Col 2</Table.Head>
-          <Table.Head>Col 3</Table.Head>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        <Table.Row>
-          <Table.Cell>Value 1.1</Table.Cell>
-          <Table.Cell>Value 1.2</Table.Cell>
-          <Table.Cell>Value 1.3</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Value 2.1</Table.Cell>
-          <Table.Cell>Value 2.2</Table.Cell>
-          <Table.Cell>Value 2.3</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Value 3.1</Table.Cell>
-          <Table.Cell>Value 3.2</Table.Cell>
-          <Table.Cell>Value 3.3</Table.Cell>
-        </Table.Row>
-      </Table.Body>
-    </Table>
 
   </PageWrapper>
 );
