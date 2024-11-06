7efe44740f34e2527012e720b7f8c0f21f5e8e47
pleber2021 <lebercontact@gmail.com>
2024-11-06 15:39:11 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -9,6 +9,21 @@ const Rank = () => (
       find all places where you should make your own changes. -- FOR LEADER
 
       PLEASE
+
+      <tr>
+        <th scope="col" class="pos" style="background: rgb(4, 83, 88);">#</th>
+        <th scope="col" class="avatar" style="background: rgb(4, 83, 88);"></th>
+        <th scope="col" class="id" style="background: rgb(63, 58, 54);">Steam ID</th>
+        <th scope="col" class="country" style="background: rgb(22, 23, 39);">Country</th>
+        <th scope="col" class="stat level relevant" data-type="XP" style="background: rgb(4, 83, 88);"> 
+          "Level"
+          ::after
+        </th>
+        <th scope="col" class="stat games" data-type="G" style="background: rgb(22, 23, 39);">Games</th>
+        <th scope="col" class="stat badges" data-type="B" style="background: rgb(4, 83, 88);">Badges</th>
+        <th scope="col" class="stat playtime" data-type="PT" style="background: rgb(7, 63, 54);">Playtime (H)</th>
+        <th scope="col" class="stat age" data-type="A" style="background: rgb(63, 58, 54);">Years</th>
+      </tr>
     </p>
   </PageWrapper>
 );
