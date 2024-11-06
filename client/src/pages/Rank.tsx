a914ac9361be21077647bd2580e329ae6018634f
pleber2021 <lebercontact@gmail.com>
2024-11-06 15:53:14 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -6,10 +6,7 @@ import type { JSX } from 'react';
 const Rank = () => (
   <PageWrapper title="Rank">
     <p className="tc f4 fw4 w-70">
-      This is a skeleton that can you can use to build a web app. It uses a
-      React frontend, an express backend, and a MongoDB database. You can deploy
-      it to Vercel for free. In your editor, search for <code>skeleton</code> to
-      find all places where you should make your own changes. -- FOR LEADER
+      LEADERBOARD
     </p>
 
     <Table>
