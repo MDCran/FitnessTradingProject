cb8489c7c6b8d9cad0937af6237bd58ded8cedbf
pleber2021 <lebercontact@gmail.com>
2024-11-06 16:01:43 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,5 +1,6 @@
 import PageWrapper from "src/components/PageWrapper";
-
+import { Table } from 'geist/components';
+import type { JSX } from 'react';
 
 const Rank = () => (
   <PageWrapper title="Rank">
@@ -7,7 +8,39 @@ const Rank = () => (
       LEADERBOARD
       For testing
     </p>
+
   </PageWrapper>
 );
+
+export function Component(): JSX.Element {
+  return (
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          <Table.Head>Col 1</Table.Head>
+          <Table.Head>Col 2</Table.Head>
+          <Table.Head>Col 3</Table.Head>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>Value 1.1</Table.Cell>
+          <Table.Cell>Value 1.2</Table.Cell>
+          <Table.Cell>Value 1.3</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>Value 2.1</Table.Cell>
+          <Table.Cell>Value 2.2</Table.Cell>
+          <Table.Cell>Value 2.3</Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell>Value 3.1</Table.Cell>
+          <Table.Cell>Value 3.2</Table.Cell>
+          <Table.Cell>Value 3.3</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table>
+  );
+}
 
 export default Rank;
