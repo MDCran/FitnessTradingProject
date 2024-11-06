7aae0ba1a1f83c565850a2abfc9b06184efc188f
pleber2021 <lebercontact@gmail.com>
2024-11-06 15:46:15 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -7,26 +7,7 @@ const Rank = () => (
       React frontend, an express backend, and a MongoDB database. You can deploy
       it to Vercel for free. In your editor, search for <code>skeleton</code> to
       find all places where you should make your own changes. -- FOR LEADER
-
-      PLEASE2
-
-      
     </p>
-    
-    <tr>
-        <th scope="col" class="pos" style="background: rgb(4, 83, 88);">#</th>
-        <th scope="col" class="avatar" style="background: rgb(4, 83, 88);"></th>
-        <th scope="col" class="id" style="background: rgb(63, 58, 54);">Steam ID</th>
-        <th scope="col" class="country" style="background: rgb(22, 23, 39);">Country</th>
-        <th scope="col" class="stat level relevant" data-type="XP" style="background: rgb(4, 83, 88);"> 
-          "Level"
-          ::after
-        </th>
-        <th scope="col" class="stat games" data-type="G" style="background: rgb(22, 23, 39);">Games</th>
-        <th scope="col" class="stat badges" data-type="B" style="background: rgb(4, 83, 88);">Badges</th>
-        <th scope="col" class="stat playtime" data-type="PT" style="background: rgb(7, 63, 54);">Playtime (H)</th>
-        <th scope="col" class="stat age" data-type="A" style="background: rgb(63, 58, 54);">Years</th>
-      </tr>
   </PageWrapper>
 );
 
