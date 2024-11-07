021ceeb48781754d406023030b6afdedc0a64e19
pleber2021 <lebercontact@gmail.com>
2024-11-07 17:04:30 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -3,11 +3,8 @@ import PageWrapper from "src/components/PageWrapper";
 const Rank = () => (
   <PageWrapper title="Rank">
     <p className="tc f4 fw4 w-70">
-      You can search for <code>skeleton</code> in your editor to find all places
-      where you should make your own changes. Check out the{" "}
-      <code>README.md</code> for more info.
+      Testing for leaderboard
     </p>
-
 
     <div className="app-container">
       <table border={1}>
@@ -18,7 +15,7 @@ const Rank = () => (
             <th>Score</th>
           </tr>
         </thead>
-        <tbody color="grey">
+        <tbody color="purple">
           <tr>
             <td color="gray">1</td>
             <td color="gray">2</td>
