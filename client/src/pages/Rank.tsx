f765c290b8bd3d56bbd8f7738821ee2727ee1b03
pleber2021 <lebercontact@gmail.com>
2024-11-07 15:57:04 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,4 +1,15 @@
 import PageWrapper from "src/components/PageWrapper";
+// Filename - App.js
+import './App.css';
+
+// Example of a data array that
+// you might receive from an API
+const data = [
+    { name: "Anom", age: 19, gender: "Male" },
+    { name: "Megha", age: 19, gender: "Female" },
+    { name: "Subham", age: 25, gender: "Male" },
+]
+
 
 const Rank = () => (
   <PageWrapper title="Rank">
@@ -7,6 +18,30 @@ const Rank = () => (
       where you should make your own changes. Check out the{" "}
       <code>README.md</code> for more info.
     </p>
+
+
+    return (
+        <div className="App">
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Gender</th>
+                </tr>
+                {data.map((val, key) => {
+                    return (
+                        <tr key={key}>
+                            <td>{val.name}</td>
+                            <td>{val.age}</td>
+                            <td>{val.gender}</td>
+                        </tr>
+                    )
+                })}
+            </table>
+        </div>
+    );
+
+
   </PageWrapper>
 );
 
