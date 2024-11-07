475c686b5dc0822d77bb01f721f2256ef358b078
pleber2021 <lebercontact@gmail.com>
2024-11-07 15:54:59 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,50 +1,13 @@
 import PageWrapper from "src/components/PageWrapper";
-import './App.css';
-
-// Example of a data array that
-// you might receive from an API
-// const data = [
-//   { name: "Anom", age: 19, gender: "Male" },
-//   { name: "Megha", age: 19, gender: "Female" },
-//   { name: "Subham", age: 25, gender: "Male" },
-// ]
-
 
 const Rank = () => (
   <PageWrapper title="Rank">
-    {/* <p className="tc f4 fw4 w-70">
-      LEADERBOARD
-      For testing table
-    </p> */}
-    <p>
-      rip 2
+    <p className="tc f4 fw4 w-70">
+      You can search for <code>skeleton</code> in your editor to find all places
+      where you should make your own changes. Check out the{" "}
+      <code>README.md</code> for more info.
     </p>
-
   </PageWrapper>
 );
-
-// function Rank() {
-//   return (
-//       <div className="App">
-//           <table>
-//               <tr>
-//                   <th>Name</th>
-//                   <th>Age</th>
-//                   <th>Gender</th>
-//               </tr>
-//               {data.map((val, key) => {
-//                   return (
-//                       <tr key={key}>
-//                           <td>{val.name}</td>
-//                           <td>{val.age}</td>
-//                           <td>{val.gender}</td>
-//                       </tr>
-//                   )
-//               })}
-//           </table>
-//       </div>
-//   );
-// }
-
 
 export default Rank;
