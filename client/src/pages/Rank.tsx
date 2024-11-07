f9806d7ff1da1ab41a2b8e9dd11e5dbb99bed896
pleber2021 <lebercontact@gmail.com>
2024-11-07 15:52:56 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -3,11 +3,11 @@ import './App.css';
 
 // Example of a data array that
 // you might receive from an API
-const data = [
-  { name: "Anom", age: 19, gender: "Male" },
-  { name: "Megha", age: 19, gender: "Female" },
-  { name: "Subham", age: 25, gender: "Male" },
-]
+// const data = [
+//   { name: "Anom", age: 19, gender: "Male" },
+//   { name: "Megha", age: 19, gender: "Female" },
+//   { name: "Subham", age: 25, gender: "Male" },
+// ]
 
 
 const Rank = () => (
