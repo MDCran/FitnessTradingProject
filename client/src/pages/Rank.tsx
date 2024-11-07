90dce20399349a3f97db90b895dd71656688ecc5
pleber2021 <lebercontact@gmail.com>
2024-11-07 16:01:31 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,9 +1,9 @@
 import PageWrapper from "src/components/PageWrapper";
-// Filename - App.js
+// // Filename - App.js
 import './App.css';
 
-// Example of a data array that
-// you might receive from an API
+// // Example of a data array that
+// // you might receive from an API
 const data = [
     { name: "Anom", age: 19, gender: "Male" },
     { name: "Megha", age: 19, gender: "Female" },
@@ -23,19 +23,15 @@ const Rank = () => (
     <div>
       <table>
           <tr>
-              <th>Name</th>
-              <th>Age</th>
-              <th>Gender</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
           </tr>
-          {data.map((val, key) => {
-              return (
-                <tr key={key}>
-                  <td>{val.name}</td>
-                  <td>{val.age}</td>
-                  <td>{val.gender}</td>
-                </tr>
-              )
-          })}
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+            </tr>
         </table>
     </div>
 
