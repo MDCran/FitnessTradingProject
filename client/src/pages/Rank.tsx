a5af57602538e7f304dbfab0c70667143051f0c4
pleber2021 <lebercontact@gmail.com>
2024-11-07 16:02:10 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,15 +1,4 @@
 import PageWrapper from "src/components/PageWrapper";
-// // Filename - App.js
-import './App.css';
-
-// // Example of a data array that
-// // you might receive from an API
-const data = [
-    { name: "Anom", age: 19, gender: "Male" },
-    { name: "Megha", age: 19, gender: "Female" },
-    { name: "Subham", age: 25, gender: "Male" },
-]
-
 
 const Rank = () => (
   <PageWrapper title="Rank">
