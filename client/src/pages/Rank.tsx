ba68fbabb57337a0554c89bfa145509b56b08dd2
pleber2021 <lebercontact@gmail.com>
2024-11-07 13:57:14 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -3,11 +3,28 @@ import PageWrapper from "src/components/PageWrapper";
 
 const Rank = () => (
   <PageWrapper title="Rank">
-    <p className="tc f4 fw4 w-70">
+    {/* <p className="tc f4 fw4 w-70">
       LEADERBOARD
       For testing table
-    </p>
-
+    </p> */}
+    
+    <table>
+      <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+      </tr>
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+      </tr>
+  </table>
 
   </PageWrapper>
 );
