23503218c931f5802b71d628e71cb5a55176a860
Bruno Fregoso <bruno88756@gmail.com>
2024-11-18 02:29:30 -0500

    website should be fine again, please try to work on different branches or test before pushing to main

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,17 +1,17 @@
-import * as React from 'react';
+/*import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow'
-import Paper from '@mui/material/Paper';
+import Paper from '@mui/material/Paper';*/
 
 import PageWrapper from "src/components/PageWrapper";
 
 
 
-function createData(
+/*function createData(
   name: string,
   calories: number,
   fat: number,
@@ -19,17 +19,17 @@ function createData(
   protein: number,
 ) {
   return { name, calories, fat, carbs, protein };
-}
+}*/
 
-const rows = [
+/*const rows = [
   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
   createData('Eclair', 262, 16.0, 24, 6.0),
   createData('Cupcake', 305, 3.7, 67, 4.3),
   createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+];*/
 
-export default function BasicTable() {
+/*export function BasicTable() {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -61,7 +61,7 @@ export default function BasicTable() {
       </Table>
     </TableContainer>
   );
-}
+}*/
 
 const Rank = () => (
   <PageWrapper title="Rank">
