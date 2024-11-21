1be6abcd2440d82555101342bcd7dd512b8b4f14
pleber2021 <lebercontact@gmail.com>
2024-11-21 13:39:33 -0500

    table

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,42 +1,73 @@
 
 
 // import React, { useMemo } from 'react';
-// import { useTable } from 'react-table';
+
 // import { COLUMNS } from
 
 
 // import React, { useState, useEffect } from "react";
 
-
+import { useTable } from 'react-table';
+import React, { useState, useEffect, useMemo } from "react";
+import axios from "axios";
 import PageWrapper from "src/components/PageWrapper";
 
 
 const data = [
-    { name: "Anom - Tqwdqwd", age: 200, gender: "Male" },
-    { name: "Megha - Tqwdqdqw", age: 400, gender: "Female" },
-    { name: "Subham - Tqwdqwd", age: 700, gender: "Male" },
-]
+  { name: 'Product 1', price: 10.99 },
+  { name: 'Product 2', price: 20.5 },
+];
+
+const columns = [
+  { Header: 'Name', accessor: 'name' },
+  {
+    Header: 'Price',
+    accessor: 'price',
+    Cell: ({ value }) => {
+      return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(value);
+    },
+  },
+];
+
+function MyTable() {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow, 
+  } = useTable({ columns, data });
 
 
 const Rank = () => (
   <PageWrapper title="Rank">
     <p className="tc f4 fw4 w-70">
-    <table>
-      <tr>
-        <th>Name</th>
-        <th>Age</th>
-        <th>Gender</th>
-      </tr>
-      {data.map((val, key) => {
-        return (
-          <tr key={key}>
-              <td>{val.name}</td>
-              <td>{val.age}</td>
-              <td>{val.gender}</td>
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
           </tr>
-          )
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              ))}
+            </tr>
+          );
         })}
-      </table>
+      </tbody>
+    </table>
     </p>
   </PageWrapper>
 );
