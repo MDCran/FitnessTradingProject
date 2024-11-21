09572134fb996db18126c38181b0144204cc1e10
pleber2021 <lebercontact@gmail.com>
2024-11-21 12:57:38 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,13 +1,37 @@
 
-// import React, { useMemo } from "react";
-// import { useTable } from "react-table";
+// import React, { useMemo } from 'react';
+// import { useTable } from 'react-table';
+// import { COLUMNS } from
+
+// import React, { useState, useEffect } from "react";
 
 import PageWrapper from "src/components/PageWrapper";
 
 const Rank = () => (
   <PageWrapper title="Rank">
     <p className="tc f4 fw4 w-70">
-      Trying out table
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+        </tr>
+        <tr>
+            <td>Anom</td>
+            <td>19</td>
+            <td>Male</td>
+        </tr>
+        <tr>
+            <td>Megha</td>
+            <td>19</td>
+            <td>Female</td>
+        </tr>
+        <tr>
+            <td>Subham</td>
+            <td>25</td>
+            <td>Male</td>
+        </tr>
+    </table>
     </p>
   </PageWrapper>
 );
