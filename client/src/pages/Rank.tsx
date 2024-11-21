20aef8872749d227845d888819eb0247d2c6ce7a
pleber2021 <lebercontact@gmail.com>
2024-11-21 13:31:13 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -12,9 +12,9 @@ import PageWrapper from "src/components/PageWrapper";
 
 
 const data = [
-    { name: "Anom - T", age: 200, gender: "Male" },
-    { name: "Megha - T", age: 400, gender: "Female" },
-    { name: "Subham - T", age: 700, gender: "Male" },
+    { name: "Anom - Tqwdqwd", age: 200, gender: "Male" },
+    { name: "Megha - Tqwdqdqw", age: 400, gender: "Female" },
+    { name: "Subham - Tqwdqwd", age: 700, gender: "Male" },
 ]
 
 
