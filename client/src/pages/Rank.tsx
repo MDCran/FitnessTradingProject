926739cc4cad03a137e38c0fc9835a81b4bc35fc
pleber2021 <lebercontact@gmail.com>
2024-11-21 13:28:53 -0500

    fix

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -10,126 +10,39 @@
 
 import PageWrapper from "src/components/PageWrapper";
 
-// This could be on diff script
-interface IEmployee {  
-  id: number,  
-  firstName: string;  
-  lastName: string;  
-  email: string;  
-  department: string;  
-  dateJoined: string;  
-}
 
-import { ColumnDef } from "@tanstack/react-table";  
-// import { IEmployee } from "../../utils/types";
+const data = [
+    { name: "Anom - T", age: 200, gender: "Male" },
+    { name: "Megha - T", age: 400, gender: "Female" },
+    { name: "Subham - T", age: 700, gender: "Male" },
+]
 
-export const COLUMNS: ColumnDef<IEmployee>[] = [  
-  {  
-    header: "ID",  
-    accessorKey: "id_number",  
-  },  
-  {  
-    header: "First Name",  
-    accessorKey: "firstName",  
-  },  
-  {  
-    header: "Last Name",  
-    accessorKey: "lastName",  
-  },  
-  {  
-    header: "Email",  
-    accessorKey: "email",  
-  },  
-  {  
-    header: "Department",  
-    accessorKey: "department",  
-  },  
-  {  
-    header: "Date Joined",  
-    accessorKey: "dateJoined",  
-  },  
-];
-
-import {  
-  useReactTable,  
-  getCoreRowModel,  
-  flexRender,  
-} from "@tanstack/react-table";  
-import {  
-  Table,  
-  TableContainer,  
-  Tbody,  
-  Td,  
-  Tfoot,  
-  Th,  
-  Thead,  
-  Tr,  
-} from "@chakra-ui/react";
-
-const ReactTable = () => {  
-  const [data, setData] = useState<IEmployee[]>([]);
-
-  // useEffect() to fetch data ...
-
-  const columns = useMemo(() => COLUMNS, []);
-
-  const table = useReactTable({  
-    columns,  
-    data,  
-    getCoreRowModel: getCoreRowModel(),  
-  });
-
-  return <div>...</div>;  
-};
 
 const Rank = () => (
   <PageWrapper title="Rank">
     <p className="tc f4 fw4 w-70">
-
-    const { getHeaderGroups, getRowModel, getFooterGroups } = table;
-
-
-      <TableContainer p={8}>  
-      <Heading>React Table Example</Heading>  
-      <Table>  
-        <Thead>  
-          {getHeaderGroups().map((headerGroup) => (  
-            <Tr key={headerGroup.id}>  
-              {headerGroup.headers.map((header) => (  
-                <Th key={header.id}>  
-                  {flexRender(header.column.columnDef.header, header.getContext())}  
-                </Th>  
-              ))}  
-            </Tr>  
-          ))}  
-        </Thead>  
-        <Tbody>  
-          {getRowModel().rows.map((row) => (  
-            <Tr key={row.id}>  
-              {row.getVisibleCells().map((cell) => (  
-                <Td key={cell.id}>  
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}  
-                </Td>  
-              ))}  
-            </Tr>  
-          ))}  
-        </Tbody>  
-        <Tfoot>  
-          {getFooterGroups().map((footerGroup) => (  
-            <Tr key={footerGroup.id}>  
-              {footerGroup.headers.map((header) => (  
-                <Th key={header.id}>  
-                  {flexRender(header.column.columnDef.footer, header.getContext())}  
-                </Th>  
-              ))}  
-            </Tr>  
-          ))}  
-        </Tfoot>  
-      </Table>  
-    </TableContainer>  
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Gender</th>
+      </tr>
+      {data.map((val, key) => {
+        return (
+          <tr key={key}>
+              <td>{val.name}</td>
+              <td>{val.age}</td>
+              <td>{val.gender}</td>
+          </tr>
+          )
+        })}
+      </table>
     </p>
   </PageWrapper>
 );
 
 
 export default Rank;
+
+
+
