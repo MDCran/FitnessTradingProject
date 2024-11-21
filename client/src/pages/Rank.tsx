fb6f040feec44301b098d3045502f69efc43bf0d
Michael C. <mdcranberry@gmail.com>
2024-11-21 15:06:52 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,23 +1,72 @@
 import "./App.css";
 // import fakeData from "./MOCK_DATA.json";
 import * as React from "react";
-import { useTable } from "react-table";
-import { JSX } from "react/jsx-runtime";
+import { useTable, Column } from "react-table";
 import PageWrapper from "src/components/PageWrapper";
 
-function Rank() {
-  // const data = React.useMemo(() => fakeData, []);
-  const data = 
-    [
-      {"id":1,"first_name":"Millicent","last_name":"Whatham","email":"mwhatham0@comsenz.com","gender":"Female","university":"Samarkand State University"},
-      {"id":2,"first_name":"Siward","last_name":"Amberger","email":"samberger1@behance.net","gender":"Male","university":"Institute of Industrial Electronics Engineering"},
-      {"id":3,"first_name":"Sheree","last_name":"Madeley","email":"smadeley2@google.com","gender":"Female","university":"Kateb Institute of Higher Education"},
-      {"id":4,"first_name":"Egor","last_name":"Downing","email":"edowning3@nymag.com","gender":"Male","university":"Universidad de Concepción del Uruguay"},
-      {"id":5,"first_name":"Donn","last_name":"Wilce","email":"dwilce4@answers.com","gender":"Male","university":"State University of New York at Binghamton"},
-      {"id":6,"first_name":"Kenon","last_name":"Jersch","email":"kjersch5@youtu.be","gender":"Male","university":"Université de Nantes"},
-    ]
+type RowData = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+  university: string;
+};
 
-  const columns = React.useMemo(
+function Rank() {
+  const data: RowData[] = [
+    {
+      id: 1,
+      first_name: "Millicent",
+      last_name: "Whatham",
+      email: "mwhatham0@comsenz.com",
+      gender: "Female",
+      university: "Samarkand State University",
+    },
+    {
+      id: 2,
+      first_name: "Siward",
+      last_name: "Amberger",
+      email: "samberger1@behance.net",
+      gender: "Male",
+      university:
+        "Institute of Industrial Electronics Engineering",
+    },
+    {
+      id: 3,
+      first_name: "Sheree",
+      last_name: "Madeley",
+      email: "smadeley2@google.com",
+      gender: "Female",
+      university: "Kateb Institute of Higher Education",
+    },
+    {
+      id: 4,
+      first_name: "Egor",
+      last_name: "Downing",
+      email: "edowning3@nymag.com",
+      gender: "Male",
+      university: "Universidad de Concepción del Uruguay",
+    },
+    {
+      id: 5,
+      first_name: "Donn",
+      last_name: "Wilce",
+      email: "dwilce4@answers.com",
+      gender: "Male",
+      university: "State University of New York at Binghamton",
+    },
+    {
+      id: 6,
+      first_name: "Kenon",
+      last_name: "Jersch",
+      email: "kjersch5@youtu.be",
+      gender: "Male",
+      university: "Université de Nantes",
+    },
+  ];
+
+  const columns: Column<RowData>[] = React.useMemo(
     () => [
       {
         Header: "ID",
@@ -47,40 +96,42 @@ function Rank() {
     []
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
-
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable<RowData>({ columns, data });
 
   return (
-    <PageWrapper title = "Rank">
-    <div className="Rank">
-      <div className="container">
-        <table {...getTableProps()}>
-          <thead>
-            {headerGroups.map((headerGroup: { getHeaderGroupProps: () => JSX.IntrinsicAttributes & React.ClassAttributes<HTMLTableRowElement> & React.HTMLAttributes<HTMLTableRowElement>; headers: any[]; }) => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column: { getHeaderProps: () => JSX.IntrinsicAttributes & React.ClassAttributes<HTMLTableHeaderCellElement> & React.ThHTMLAttributes<HTMLTableHeaderCellElement>; render: (arg0: string) => string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }) => (
-                  <th {...column.getHeaderProps()}>
-                    {column.render("Header")}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row: { getRowProps: () => JSX.IntrinsicAttributes & React.ClassAttributes<HTMLTableRowElement> & React.HTMLAttributes<HTMLTableRowElement>; cells: any[]; }) => {
-              prepareRow(row);
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map((cell: { getCellProps: () => JSX.IntrinsicAttributes & React.ClassAttributes<HTMLTableDataCellElement> & React.TdHTMLAttributes<HTMLTableDataCellElement>; render: (arg0: string) => string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }) => (
-                    <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
+    <PageWrapper title="Rank">
+      <div className="Rank">
+        <div className="container">
+          <table {...getTableProps()}>
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <th {...column.getHeaderProps()}>
+                      {column.render("Header")}
+                    </th>
                   ))}
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
+              ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row) => {
+                prepareRow(row);
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell) => (
+                      <td {...cell.getCellProps()}>
+                        {cell.render("Cell")}
+                      </td>
+                    ))}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
     </PageWrapper>
   );
 }
