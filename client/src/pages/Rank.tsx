75da1240940fd28a7682f4e768d3ec81c70d9d4d
pleber2021 <lebercontact@gmail.com>
2024-11-21 15:22:31 -0500

    Update Rank.tsx

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,107 +1,115 @@
 import "./App.css";
 // import fakeData from "./MOCK_DATA.json";
-// import * as React from "react";
-// import { useTable, Column } from "react-table";
+import * as React from "react";
+import { useTable, Column } from "react-table";
 import PageWrapper from "src/components/PageWrapper";
 
-// type RowData = {
-//   id: number;
-//   first_name: string;
-//   last_name: string;
-//   email: string;
-//   gender: string;
-//   university: string;
-// };
+type RowData = {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+  university: string;
+};
 
 function Rank() {
-//   const data: RowData[] = [
-//     {
-//       id: 1,
-//       first_name: "Millicent",
-//       last_name: "Whatham",
-//       email: "mwhatham0@comsenz.com",
-//       gender: "Female",
-//       university: "Samarkand State University",
-//     },
-//     {
-//       id: 2,
-//       first_name: "Siward",
-//       last_name: "Amberger",
-//       email: "samberger1@behance.net",
-//       gender: "Male",
-//       university:
-//         "Institute of Industrial Electronics Engineering",
-//     },
-//     {
-//       id: 3,
-//       first_name: "Sheree",
-//       last_name: "Madeley",
-//       email: "smadeley2@google.com",
-//       gender: "Female",
-//       university: "Kateb Institute of Higher Education",
-//     },
-//     {
-//       id: 4,
-//       first_name: "Egor",
-//       last_name: "Downing",
-//       email: "edowning3@nymag.com",
-//       gender: "Male",
-//       university: "Universidad de Concepción del Uruguay",
-//     },
-//     {
-//       id: 5,
-//       first_name: "Donn",
-//       last_name: "Wilce",
-//       email: "dwilce4@answers.com",
-//       gender: "Male",
-//       university: "State University of New York at Binghamton",
-//     },
-//     {
-//       id: 6,
-//       first_name: "Kenon",
-//       last_name: "Jersch",
-//       email: "kjersch5@youtu.be",
-//       gender: "Male",
-//       university: "Université de Nantes",
-//     },
-//   ];
+  const data: RowData[] = [
+    {
+      id: 1,
+      first_name: "Millicent",
+      last_name: "Whatham",
+      email: "mwhatham0@comsenz.com",
+      gender: "Female",
+      university: "Samarkand State University",
+    },
+    {
+      id: 2,
+      first_name: "Siward",
+      last_name: "Amberger",
+      email: "samberger1@behance.net",
+      gender: "Male",
+      university:
+        "Institute of Industrial Electronics Engineering",
+    },
+    {
+      id: 3,
+      first_name: "Sheree",
+      last_name: "Madeley",
+      email: "smadeley2@google.com",
+      gender: "Female",
+      university: "Kateb Institute of Higher Education",
+    },
+    {
+      id: 4,
+      first_name: "Egor",
+      last_name: "Downing",
+      email: "edowning3@nymag.com",
+      gender: "Male",
+      university: "Universidad de Concepción del Uruguay",
+    },
+    {
+      id: 5,
+      first_name: "Donn",
+      last_name: "Wilce",
+      email: "dwilce4@answers.com",
+      gender: "Male",
+      university: "State University of New York at Binghamton",
+    },
+    {
+      id: 6,
+      first_name: "Kenon",
+      last_name: "Jersch",
+      email: "kjersch5@youtu.be",
+      gender: "Male",
+      university: "Université de Nantes",
+    },
+  ];
 
-//   const columns: Column<RowData>[] = React.useMemo(
-//     () => [
-//       {
-//         Header: "ID",
-//         accessor: "id",
-//       },
-//       {
-//         Header: "First Name",
-//         accessor: "first_name",
-//       },
-//       {
-//         Header: "Last Name",
-//         accessor: "last_name",
-//       },
-//       {
-//         Header: "Email",
-//         accessor: "email",
-//       },
-//       {
-//         Header: "Gender",
-//         accessor: "gender",
-//       },
-//       {
-//         Header: "University",
-//         accessor: "university",
-//       },
-//     ],
-//     []
-//   );
+  const columns: Column<RowData>[] = React.useMemo(
+    () => [
+      {
+        Header: "ID",
+        accessor: "id",
+      },
+      {
+        Header: "First Name",
+        accessor: "first_name",
+      },
+      {
+        Header: "Last Name",
+        accessor: "last_name",
+      },
+      {
+        Header: "Email",
+        accessor: "email",
+      },
+      {
+        Header: "Gender",
+        accessor: "gender",
+      },
+      {
+        Header: "University",
+        accessor: "university",
+      },
+    ],
+    []
+  );
 
-  // const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-  //   useTable<RowData>({ columns, data });
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable<RowData>({ columns, data });
 
   return (
     <PageWrapper title="Rank">
-      {/* <div className="Rank">
+
+      <p className="tc f4 fw4 w-70">
+      This is a skeleton that can you can use to build a web app. It uses a
+      React frontend, an express backend, and a MongoDB database. You can deploy
+      it to Vercel for free. In your editor, search for <code>skeleton</code> to
+      find all places where you should make your own changes. -- TEST
+      </p>
+
+      <div className="Rank">
         <div className="container">
           <table {...getTableProps()}>
             <thead>
@@ -131,14 +139,8 @@ function Rank() {
             </tbody>
           </table>
         </div>
-      </div> */}
+      </div>
 
-      <p className="tc f4 fw4 w-70">
-      This is a skeleton that can you can use to build a web app. It uses a
-      React frontend, an express backend, and a MongoDB database. You can deploy
-      it to Vercel for free. In your editor, search for <code>skeleton</code> to
-      find all places where you should make your own changes. -- TEST
-      </p>
     </PageWrapper>
   );
 }
