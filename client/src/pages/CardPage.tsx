40bd88ac3a45f91693dd020dc790de88c302c295
pleber2021 <lebercontact@gmail.com>
2024-12-02 15:56:38 -0500

    card again

--- client/src/pages/CardPage.tsx
+++ client/src/pages/CardPage.tsx
@@ -4,6 +4,11 @@ import './cardpage.css';
 const CardPage = () => (  
   <PageWrapper title="CardPage">  
    <div className="user-card">  
+
+    <div className="card-logo">  
+      <img src="fitknights_logo.png" alt="Logo" />  
+    </div>
+
     <div className="card-header">  
       <h2>Test</h2>
       <h3>Aura Points: 60</h3>  
@@ -12,6 +17,9 @@ const CardPage = () => (
     <div className="card-image">  
       <img src="https://images.unsplash.com/photo-1458349726531-234ad56ba80f?q=80&w=2362&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="User" />  
     </div>  
+
+    <div className="bottom-bar">
+    </div>
   
     <div className="card-description">  
       <h4>Rank: "input rank"</h4>
