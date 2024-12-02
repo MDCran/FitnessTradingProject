de22d4c85932cb7b48b00fddbb5766c9d882c531
pleber2021 <lebercontact@gmail.com>
2024-12-02 15:27:12 -0500

    Update CardPage.tsx

--- client/src/pages/CardPage.tsx
+++ client/src/pages/CardPage.tsx
@@ -10,7 +10,7 @@ const CardPage = () => (
     </div>  
   
     <div className="card-image">  
-      <img src="user.png" alt="User" />  
+      <img src="https://images.unsplash.com/photo-1458349726531-234ad56ba80f?q=80&w=2362&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="User" />  
     </div>  
   
     <div className="card-description">  
