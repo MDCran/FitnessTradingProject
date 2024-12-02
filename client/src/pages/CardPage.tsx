89886975c36d0591e420241369f2590ee557ddfd
pleber2021 <lebercontact@gmail.com>
2024-12-02 14:49:25 -0500

    card page

--- client/src/pages/CardPage.tsx
+++ client/src/pages/CardPage.tsx
@@ -1,11 +1,24 @@
-import PageWrapper from "src/components/PageWrapper";
-
-const CardPage = () => (
-  <PageWrapper title="CardPage">
-    <p>
-      card here 
-    </p>
-  </PageWrapper>
-);
-
+import PageWrapper from "src/components/PageWrapper";  
+import './CardPage.css';  
+  
+const CardPage = () => (  
+  <PageWrapper title="CardPage">  
+   <div className="user-card">  
+    <div className="card-header">  
+      <h2>Test</h2>  
+      <p>Aura Points: 60</p>  
+    </div>  
+  
+    <div className="card-image">  
+      <img src="user.png" alt="User" />  
+    </div>  
+  
+    <div className="card-description">  
+      <p>Rank: "input rank"</p>  
+      <p>Could put the latest completed challenges here.</p>  
+    </div>  
+   </div>  
+  </PageWrapper>  
+);  
+  
 export default CardPage;
