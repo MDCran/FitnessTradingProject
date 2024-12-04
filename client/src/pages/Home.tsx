cacacc185203c9c0e0288c381ff1d16e5c6aa30a
shanemcder <shanemcdermott200@gmail.com>
2024-12-03 23:35:10 -0500

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import PageWrapper from "src/components/PageWrapper";
+import { Link } from "react-router-dom";
 import "../css/Home.css";
 
 interface Challenge {
